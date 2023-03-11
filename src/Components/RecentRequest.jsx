d8f5c3eaba078c0d27d82792a48a8adde96ce46a
Developer@Miles <86254927+zenith-noble01@users.noreply.github.com>
2023-03-11 18:09:12 +0100

    Update RecentRequest.jsx

--- src/Components/RecentRequest.jsx
+++ src/Components/RecentRequest.jsx
@@ -1,23 +1,7 @@
 import React from "react";
-import { recentRequest } from "../Constants";
 
 const RecentRequest = () => {
-  return (
-    <div className="recent__requests">
-      {recentRequest.map((request, index) => (
-        <div className="request__container" key={index}>
-          <div className="request__user">
-            <img src={request.profile} alt="" />
-            <p>{request.name}</p>
-          </div>
-          <div className="request__actions">
-            <button>Acept</button>
-            <button>Decline</button>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <div>RecentRequest</div>;
 };
 
 export default RecentRequest;
