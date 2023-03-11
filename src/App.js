bfc6bb1c0a63606ffabd6dbdff63c69262c1a28c
zenith-noble01 <webdev686@gmail.com>
2023-03-11 17:28:15 +0100

    react social media starter code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./Styles/app.scss";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <div className="App">appp</div>;
+  return <div className="App">App</div>;
 }
 
 export default App;
